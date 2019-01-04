16cd19405437ece2ba8762b6dc0635581bde9ebf
Rayn.Gosling <wenleigang0926@163.com>
2019-01-04 14:02:44 +0800

    分布式配置中心 server client config

---
+++ springcloud-configclient/src/main/java/com/chaosquark/springcloudconfigclient/SpringcloudConfigclientApplication.java
@@ -0,0 +1,39 @@
+package com.chaosquark.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RefreshScope
+@EnableEurekaClient
+public class SpringcloudConfigclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigclientApplication.class, args);
+    }
+
+    /*@Value("${foo}")
+    private String foo;
+
+    @RequestMapping(value = "/hi")
+    public String home() {
+        return foo;
+    }*/
+
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping(value = "/hi")
+    public String home() {
+        return environment.getProperty("foo", "undefiend");
+    }
+}
+
